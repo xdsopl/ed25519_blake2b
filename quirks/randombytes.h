a25eeefcdbee3eb3a1c3608983ea0296ce2ef4a4
Ahmet Inan <xdsopl@gmail.com>
2024-03-31 08:53:58 +0200

    take random bytes from the random_bytes array

--- quirks/randombytes.h
+++ quirks/randombytes.h
@@ -6,8 +6,12 @@ Copyright 2024 Ahmet Inan <xdsopl@gmail.com>
 
 #pragma once
 
+#include "string.h"
+
+unsigned char random_bytes[32];
+
 static void randombytes(unsigned char *buf, unsigned long long num)
 {
-	while (num--)
-		*buf++ = num;
+	memcpy(buf, random_bytes, num);
+	memset(random_bytes, 0, num);
 }
